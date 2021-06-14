546b042991f1ed7b39cb0e5d8e135b5c289c0222
hiyori13 <hiyo3chapi10@gmail.com>
2021-06-14 10:27:41 +0900

    implement action and action creator

---
+++ src/actions/index.js
@@ -0,0 +1,10 @@
+export const INCREMET = "INCREMET"
+export const DECREMET = "DECREMET"
+
+export const increment = () =>({
+    type: INCREMET
+})
+
+export const decrement = () =>({
+    type: DECREMET
+})
